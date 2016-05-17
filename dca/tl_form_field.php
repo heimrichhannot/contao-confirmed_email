df4dd0cdbdf2bdb11e9efbcf1d41fb86d1402c1f
Dennis Patzer <d.patzer@heimrich-hannot.de>
2016-05-17 14:26:22 +0200

    added rgxp field

--- dca/tl_form_field.php
+++ dca/tl_form_field.php
@@ -3,4 +3,4 @@
 /**
  * Palettes
  */
-$GLOBALS['TL_DCA']['tl_form_field']['palettes']['confirmed_email'] = '{type_legend},type,name,label;{fconfig_legend},mandatory,placeholder;{expert_legend:hide},class,maxlength,accesskey,tabindex,value;{submit_legend},addSubmit';
+$GLOBALS['TL_DCA']['tl_form_field']['palettes']['confirmed_email'] = '{type_legend},type,name,label;{fconfig_legend},mandatory,rgxp,placeholder;{expert_legend:hide},class,maxlength,accesskey,tabindex,value;{submit_legend},addSubmit';
